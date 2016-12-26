6225cfed88208af555f0ae8df90fd8fc8186a9b7
Saar Wasserman <saarwasserman@gmail.com>
2016-12-26 10:18:04 +0200

    first init

---
+++ CarRental.Client.Entities/Car.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Common.Core;
+
+namespace CarRental.Client.Entities
+{
+    public class Car : TempObjectBase
+    {
+        int _CarId;
+        string _Description;
+        string _Color;
+        int _Year;
+        decimal _RentalPrice;
+        bool _CurrentlyRental;
+
+        public int CarId
+        {
+            get { return _CarId; }
+            set
+            {
+                if (_CarId != value)
+                {
+                    _CarId = value;
+                    OnPropertyChanged("CarId");
+                }
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return _Description;
+            }
+
+            set
+            {
+                _Description = value;
+            }
+        }
+
+        public string Color
+        {
+            get
+            {
+                return _Color;
+            }
+
+            set
+            {
+                _Color = value;
+            }
+        }
+
+        public int Year
+        {
+            get
+            {
+                return _Year;
+            }
+
+            set
+            {
+                _Year = value;
+            }
+        }
+
+        public decimal RentalPrice
+        {
+            get
+            {
+                return _RentalPrice;
+            }
+
+            set
+            {
+                _RentalPrice = value;
+            }
+        }
+
+        public bool CurrentlyRental
+        {
+            get
+            {
+                return _CurrentlyRental;
+            }
+
+            set
+            {
+                _CurrentlyRental = value;
+            }
+        }
+    }
+}
---
+++ Core.Common/Core/TempObjectBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Common.Core
+{
+    public class TempObjectBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            OnPropertyChanged(propertyName, true);
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName, bool makeDirty)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+            if (makeDirty)
+                _IsDirty = true;
+        }
+
+        private bool _IsDirty;
+
+        public bool IsDirty
+        {
+            get { return _IsDirty; }
+        }
+    }
+}
